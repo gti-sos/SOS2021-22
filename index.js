5c83211d78e52fd9e7bc3a688a0726e9fe90a8d8
mansegrod <mansegrod>
2021-03-20 11:16:51 +0100

    fix port 2

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ var app = express();
 var port = (process.env.port || 10000);
 
 app.use('/', express.static(path.join(__dirname, 'public')));
-
+/*
 app.get('/cool', (request, response) => {
     response.send(cool());
     console.log('New request to /cool has arrived');
 });
-
+*/
 app.get('/index', (request, response) => {
     response.send(express());       
     console.log('New request to /idex has arrived');
