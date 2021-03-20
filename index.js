3251b017d497af71a6a813fe9aeba98cad1401fd
mansegrod <mansegrod>
2021-03-20 11:10:12 +0100

    fix port

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
+var path = require('path');
+
 var app = express();
-var port = 10000;
+
+var port = (process.env.port || 10000);
+
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/cool', (request, response) => {
     response.send(cool());
     console.log('New request to /cool has arrived');
 });
 
+app.get('/index', (request, response) => {
+    response.send(express());       
+    console.log('New request to /idex has arrived');
+});
+
 app.listen(port, () => {
-    console.log('Server ready listenig on port ' + port);
+    console.log(`Server ready listenig on port ${port}`);
 });
