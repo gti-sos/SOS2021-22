75374911581df657b6790467cb529e4e9494d07c
antonioparra7 <antonioparra00@gmail.com>
2021-03-21 16:29:58 +0000

    Añadida la tabla de Excel en formato html

--- index.js
+++ index.js
@@ -25,9 +25,249 @@ app.get('/info/paawards', (request, response) => {
 	<head>
         <title>PAAWARDS</title>
     </head>
-	`);
+
+	<body>
+		<table>
+			<col class="col0">
+			<col class="col1">
+			<col class="col2">
+			<col class="col3">
+			<col class="col4">
+			<col class="col5">
+			<col class="col6">
+        	<tbody>
+			  <tr class="row0">
+				<td class="column0 style1 s">name</td>
+				<td class="column1 style1 s">year</td>
+				<td class="column2 style1 s">sport</td>
+				<td class="column3 style1 s">country</td>
+				<td class="column4 style1 s">age</td>
+				<td class="column5 style1 s">gender</td>
+				<td class="column6 style1 s">trophy</td>
+			  </tr>
+			  <tr class="row1">
+				<td class="column0 style2 s">Carlos Sainz</td>
+				<td class="column1 style3 n">2020</td>
+				<td class="column2 style4 s">Rally</td>
+				<td class="column3 style4 s">Spain</td>
+				<td class="column4 style3 n">58</td>
+				<td class="column5 style4 s">Masculino</td>
+				<td class="column6 style3 n">10</td>
+			  </tr>
+			  <tr class="row2">
+				<td class="column0 style2 s">Lindsey Vonn</td>
+				<td class="column1 style3 n">2019</td>
+				<td class="column2 style4 s">Esqui alpino</td>
+				<td class="column3 style4 s">EE.UU</td>
+				<td class="column4 style3 n">35</td>
+				<td class="column5 style4 s">Femenino</td>
+				<td class="column6 style3 n">7</td>
+			  </tr>
+			  <tr class="row3">
+				<td class="column0 style2 s">Reinhold Messner </td>
+				<td class="column1 style3 n">2018</td>
+				<td class="column2 style4 s">Alpinismo</td>
+				<td class="column3 style4 s">Italia</td>
+				<td class="column4 style3 n">74</td>
+				<td class="column5 style4 s">Masculino</td>
+				<td class="column6 style3 n">0</td>
+			  </tr>
+			  <tr class="row4">
+				<td class="column0 style2 s">Krzysztof Wielicki</td>
+				<td class="column1 style3 n">2018</td>
+				<td class="column2 style4 s">Alpinismo</td>
+				<td class="column3 style4 s">Polonia</td>
+				<td class="column4 style3 n">68</td>
+				<td class="column5 style4 s">Masculino</td>
+				<td class="column6 style3 n">0</td>
+			  </tr>
+			  <tr class="row5">
+				<td class="column0 style2 s">Selección de rugby de Nueva Zelanda</td>
+				<td class="column1 style3 n">2017</td>
+				<td class="column2 style4 s">Rugby</td>
+				<td class="column3 style4 s">Nueva Zelanda</td>
+				<td class="column4 style3 n">125</td>
+				<td class="column5 style4 s">Masculino</td>
+				<td class="column6 style3 n">100</td>
+			  </tr>
+			  <tr class="row6">
+				<td class="column0 style2 s">Javier Gómez Noya</td>
+				<td class="column1 style3 n">2016</td>
+				<td class="column2 style4 s">Triatlon</td>
+				<td class="column3 style4 s">España</td>
+				<td class="column4 style3 n">33</td>
+				<td class="column5 style4 s">Masculino</td>
+				<td class="column6 style3 n">12</td>
+			  </tr>
+			  <tr class="row7">
+				<td class="column0 style2 s">Pau Gasol</td>
+				<td class="column1 style3 n">2015</td>
+				<td class="column2 style4 s">Baloncesto</td>
+				<td class="column3 style4 s">España</td>
+				<td class="column4 style3 n">35</td>
+				<td class="column5 style4 s">Masculino</td>
+				<td class="column6 style3 n">19</td>
+			  </tr>
+			  <tr class="row8">
+				<td class="column0 style2 s">Marc Gasol</td>
+				<td class="column1 style3 n">2015</td>
+				<td class="column2 style4 s">Baloncesto</td>
+				<td class="column3 style4 s">España</td>
+				<td class="column4 style3 n">30</td>
+				<td class="column5 style4 s">Masculino</td>
+				<td class="column6 style3 n">7</td>
+			  </tr>
+			  <tr class="row9">
+				<td class="column0 style2 s">Maratón de Nueva York</td>
+				<td class="column1 style3 n">2014</td>
+				<td class="column2 style4 s">Atletismo</td>
+				<td class="column3 style4 s">EE.UU</td>
+				<td class="column4 style3 n">44</td>
+				<td class="column5 style4 s">Mixto</td>
+				<td class="column6 style3 n">0</td>
+			  </tr>
+			  <tr class="row10">
+				<td class="column0 style2 s">José María Olazábal</td>
+				<td class="column1 style3 n">2013</td>
+				<td class="column2 style4 s">Golf</td>
+				<td class="column3 style4 s">España</td>
+				<td class="column4 style3 n">47</td>
+				<td class="column5 style4 s">Masculino</td>
+				<td class="column6 style3 n">29</td>
+			  </tr>
+			  <tr class="row11">
+				<td class="column0 style2 s">Iker Casillas</td>
+				<td class="column1 style3 n">2012</td>
+				<td class="column2 style4 s">Futbol</td>
+				<td class="column3 style4 s">España</td>
+				<td class="column4 style3 n">31</td>
+				<td class="column5 style4 s">Masculino</td>
+				<td class="column6 style3 n">26</td>
+			  </tr>
+			  <tr class="row12">
+				<td class="column0 style2 s">Xavi Hernández</td>
+				<td class="column1 style3 n">2012</td>
+				<td class="column2 style4 s">Futbol</td>
+				<td class="column3 style4 s">España</td>
+				<td class="column4 style3 n">32</td>
+				<td class="column5 style4 s">Masculino</td>
+				<td class="column6 style3 n">35</td>
+			  </tr>
+			  <tr class="row13">
+				<td class="column0 style2 s">Haile Gebrselassie</td>
+				<td class="column1 style3 n">2011</td>
+				<td class="column2 style4 s">Atletismo</td>
+				<td class="column3 style4 s">Etiopía</td>
+				<td class="column4 style3 n">38</td>
+				<td class="column5 style4 s">Masculino</td>
+				<td class="column6 style3 n">6</td>
+			  </tr>
+			  <tr class="row14">
+				<td class="column0 style2 s">Selección de fútbol de España</td>
+				<td class="column1 style3 n">2010</td>
+				<td class="column2 style4 s">Futbol</td>
+				<td class="column3 style4 s">España</td>
+				<td class="column4 style3 n">90</td>
+				<td class="column5 style4 s">Masculino</td>
+				<td class="column6 style3 n">4</td>
+			  </tr>
+			  <tr class="row15">
+				<td class="column0 style2 s">Yelena Isinbáyeva</td>
+				<td class="column1 style3 n">2009</td>
+				<td class="column2 style4 s">Atletismo</td>
+				<td class="column3 style4 s">Rusia</td>
+				<td class="column4 style3 n">27</td>
+				<td class="column5 style4 s">Femenino</td>
+				<td class="column6 style3 n">6</td>
+			  </tr>
+			  <tr class="row16">
+				<td class="column0 style2 s">Rafael Nadal</td>
+				<td class="column1 style3 n">2008</td>
+				<td class="column2 style4 s">Tenis</td>
+				<td class="column3 style4 s">España</td>
+				<td class="column4 style3 n">22</td>
+				<td class="column5 style4 s">Masculino</td>
+				<td class="column6 style3 n">86</td>
+			  </tr>
+			  <tr class="row17">
+				<td class="column0 style2 s">Michael Schumacher</td>
+				<td class="column1 style3 n">2007</td>
+				<td class="column2 style4 s">Automovilismo</td>
+				<td class="column3 style4 s">Alemania</td>
+				<td class="column4 style3 n">38</td>
+				<td class="column5 style4 s">Masculino</td>
+				<td class="column6 style3 n">7</td>
+			  </tr>
+			  <tr class="row18">
+				<td class="column0 style2 s">Selección de baloncesto de España</td>
+				<td class="column1 style3 n">2006</td>
+				<td class="column2 style4 s">Baloncesto</td>
+				<td class="column3 style4 s">España</td>
+				<td class="column4 style3 n">72</td>
+				<td class="column5 style4 s">Masculino</td>
+				<td class="column6 style3 n">8</td>
+			  </tr>
+			  <tr class="row19">
+				<td class="column0 style2 s">Fernando Alonso</td>
+				<td class="column1 style3 n">2005</td>
+				<td class="column2 style4 s">Automovilismo</td>
+				<td class="column3 style4 s">España</td>
+				<td class="column4 style3 n">24</td>
+				<td class="column5 style4 s">Masculino</td>
+				<td class="column6 style3 n">6</td>
+			  </tr>
+			  <tr class="row20">
+				<td class="column0 style2 s">Hicham El Guerrouj</td>
+				<td class="column1 style3 n">2004</td>
+				<td class="column2 style4 s">Atletismo</td>
+				<td class="column3 style4 s">Marruecos</td>
+				<td class="column4 style3 n">30</td>
+				<td class="column5 style4 s">Masculino</td>
+				<td class="column6 style3 n">6</td>
+			  </tr>
+			  <tr class="row21">
+				<td class="column0 style2 s">Tour de Francia</td>
+				<td class="column1 style3 n">2003</td>
+				<td class="column2 style4 s">Ciclismo</td>
+				<td class="column3 style4 s">Francia</td>
+				<td class="column4 style3 n">100</td>
+				<td class="column5 style4 s">Masculino</td>
+				<td class="column6 style3 n">0</td>
+			  </tr>
+			  <tr class="row22">
+				<td class="column0 style2 s">Selección de fútbol de Brasil</td>
+				<td class="column1 style3 n">2002</td>
+				<td class="column2 style4 s">Futbol</td>
+				<td class="column3 style4 s">Brasil</td>
+				<td class="column4 style3 n">88</td>
+				<td class="column5 style4 s">Masculino</td>
+				<td class="column6 style3 n">21</td>
+			  </tr>
+			  <tr class="row23">
+				<td class="column0 style2 s">Manel Estiarte</td>
+				<td class="column1 style3 n">2001</td>
+				<td class="column2 style4 s">Waterpolo</td>
+				<td class="column3 style4 s">España</td>
+				<td class="column4 style3 n">40</td>
+				<td class="column5 style4 s">Masculino</td>
+				<td class="column6 style3 n">26</td>
+			  </tr>
+			  <tr class="row24">
+				<td class="column0 style2 s">Lance Armstrong</td>
+				<td class="column1 style3 n">2000</td>
+				<td class="column2 style4 s">Ciclismo</td>
+				<td class="column3 style4 s">EE.UU</td>
+				<td class="column4 style3 n">29</td>
+				<td class="column5 style4 s">Masculino</td>
+				<td class="column6 style3 n">11</td>
+			  </tr>
+			</tbody>
+    	</table>
+	</body>
+	</html>`);
     console.log('New request to /idex has arrived');
 });
+
 app.get('/info/richpp', (request, response) => {
     response.send(`<!DOCTYPE html>
     <html>
