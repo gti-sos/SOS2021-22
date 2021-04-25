e1a4ffbdf8de1717c5f105e52d9ac1d2fadc53d9
mansegrod <mansegrod>
2021-04-25 15:23:37 +0200

    tabla prueba + link frontend

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,41 @@
-<script lang="ts">
-  
-    import Router from 'svelte-routing';
-  	import grmys from "./grmys-api.svelte";
-	import paawards from "./paawards-api.svelte";
-	import richpp from "./richpp-api.svelte";
-	import Principal from './Principal.svelte';
-	
+<script>
+	//import Router from 'svelte-routing';
+	//import grmys from './grmys-api.svelte';
+	//import paawards from './paawards-api.svelte';
+	import Richpp from './Richpp.svelte';
+	//import Principal from './Principal.svelte';
+	export let name;
 </script>
 
 <main>
-	
+
+	<h1>Hello {name}!</h1>
+	<input bind:value={name}>
+
+	<Richpp/>
+
+	<a href="index_static.html">Pincha aquí para ver toda la información del proyecto</a>
+
 </main>
 
 <style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
 </style>
---
+++ src/Richpp.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { Table } from "sveltestrap";
+
+    let richmen = [];
+
+    async function getRichmen() {
+        console.log("Recopilando datos de personas ricas...");
+        const res = await fetch("/richmen");
+
+        if (res.ok) {
+            console.log("OK.");
+            const json = await res.json();
+            richmen = json;
+            console.log(`Tenemos ${richmen.length} richmen`);
+        } else {
+            console.log("Error!");
+        }
+    }
+
+    onMount(getRichmen);
+</script>
+
+<main>
+    <Table bordered>
+        <thead>
+            <tr>
+                <td>Top</td>
+                <td>Name</td>
+                <td>Fortune</td>
+                <td>Age</td>
+                <td>Country</td>
+                <td>Year</td>
+                <td>Company</td>
+            </tr>
+        </thead>
+        <tbody>
+            {#each richmen as richman}
+                <tr>
+                    <td>{richman.top}</td>
+                    <td>{richman.name}</td>
+                    <td>{richman.fortune}</td>
+                    <td>{richman.age}</td>
+                    <td>{richman.country}</td>
+                    <td>{richman.year}</td>
+                    <td>{richman.company}</td>
+                </tr>
+            {/each}
+        </tbody>
+    </Table>
+</main>
+
+<style>
+    table {
+        border: 1px solid black;
+    }
+</style>
