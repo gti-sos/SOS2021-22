53e9286333d5b9b9db458d7f94f57118906d3212
josgonrod7 <josegonzarod@gmail.com>
2021-04-26 22:07:17 +0200

    svelte sin terminar

---
+++ src/grmysApi.svelte
@@ -0,0 +1,105 @@
+<script>
+    import {Nav,
+            NavItem,
+            NavLink,
+        Button,
+        Table,
+        UncontrolledAlert} from "sveltestrap";
+
+       const botonCargar = () => {
+        loadGrmys();
+    };
+    const botonBorrar = () => {
+        deleteGrmys();
+    };     
+    let grmys = [];
+    let error = null;
+    async function loadGrmys() {
+        console.log("Loading Grmys");
+        const res = await fetch("api/v1/grmys/loadInitialData").then(function (res) {
+            if (res.ok) {
+                console.log("Ok");
+                getGrmys();
+            } 
+            else {
+                error = 404;
+                console.log("Error");
+            }
+        });
+    }
+    async function getGrmys() {
+        console.log("Fetching Grmys");
+        const res = await fetch("api/v1/grmys/");
+        if (res.ok) {
+            console.log("Ok");
+            const json = await res.json();
+            grmys = json;
+            console.log(`We have received ${grmys.length} resources.`);
+        } else {
+            console.log("Error");
+        }
+    }
+    async function deleteGrmys() {
+        console.log("Deleting data...");
+        const res = await fetch("api/v1/grmys", {
+            method: "DELETE",
+        }).then(function (res) {
+            if (res.ok) {
+                console.log("OK");
+                grmys = [];
+            } else {
+                console.log("Error");
+            }
+        });
+    }
+</script>
+
+<main>
+    <Nav>
+        <NavItem>
+            <NavLink href="/">Volver</NavLink>
+        </NavItem>
+        <NavItem>
+            <NavLink href="#" on:click={botonCargar}>Cargar Gramys</NavLink>
+        </NavItem>
+        <NavItem>
+            <NavLink href="#" on:click={botonBorrar}>Borrar Gramys</NavLink>
+        </NavItem>
+    </Nav>
+    <h2>GRAMYS</h2>
+    <Table>
+        <thead>
+            <tr>
+                <td>Ranking</td>
+                <td>Name</td>
+                <td>Award</td>
+                <td>Country</td>
+                <td>groupmember</td>
+                <td>Style</td>
+                <td>Year</td>
+            </tr>
+        </thead>
+        <tbody>
+            {#each grmys as grmy}
+                <tr>
+                    <td>{grmys}</td>
+                    <td>{grmy.year}</td>
+                    <td>{grmy.sport}</td>
+                    <td>{grmy.country}</td>
+                    <td>{grmy.age}</td>
+                    <td>{grmy.gender}</td>
+                    <td>{grmy.trophy}</td>
+                </tr>
+            {/each}
+        </tbody>
+    </Table>
+</main>
+
+<style>
+    h2 {
+        text-align: center;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+</style>
