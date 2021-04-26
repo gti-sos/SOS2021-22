2860385c1d44ec45c551d5110d2abcbbccfbea01
josgonrod7 <josegonzarod@gmail.com>
2021-04-26 16:52:12 +0200

    svelte no va

--- src/App.svelte
+++ src/App.svelte
@@ -2,23 +2,20 @@
 	
 	import Router from 'svelte-spa-router';
 	import Home from './Home.svelte';
-	import grmys from './grmys-api.svelte';
-	//import paawards from './paawards-api.svelte';
-	//import Richpp from './Richpp.svelte';
+	import grmys from './grmysApi.svelte';
+	import paawards from './paawards-api.svelte';
+	import Richpp from './Richpp.svelte';
 const routes = {
 		"/":Home,
 		"/grmys": grmys,
+		"/richpp": Richpp,
+		"/paawards": paawards,
 		"*":NotFound,
     };
 </script>
 
 <main>
-
-	
 	<Router {routes}> </Router>
-	
-
 	<a href="index_static.html">Pincha aquí para ver toda la información del proyecto</a>
-
 </main>
 
