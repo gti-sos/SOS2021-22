ce09fa8caac508ba8eb00b87e6ee2135adc88d61
josgonrod7 <josegonzarod@gmail.com>
2021-04-26 14:18:59 +0200

    svelte no va

--- src/App.svelte
+++ src/App.svelte
@@ -1,41 +1,24 @@
 <script>
-	//import Router from 'svelte-routing';
-	//import grmys from './grmys-api.svelte';
+	
+	import Router from 'svelte-spa-router';
+	import Home from './Home.svelte';
+	import grmys from './grmys-api.svelte';
 	//import paawards from './paawards-api.svelte';
-	import Richpp from './Richpp.svelte';
-	//import Principal from './Principal.svelte';
-	export let name;
+	//import Richpp from './Richpp.svelte';
+const routes = {
+		"/":Home,
+		"/grmys": grmys,
+		"*":NotFound,
+    };
 </script>
 
 <main>
 
-	<h1>Hello {name}!</h1>
-	<input bind:value={name}>
-
-	<Richpp/>
+	
+	<Router {routes}> </Router>
+	
 
 	<a href="index_static.html">Pincha aquí para ver toda la información del proyecto</a>
 
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
