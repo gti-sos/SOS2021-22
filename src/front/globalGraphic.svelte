e82e9471c1a5c4644d5c8d9d71bf60d1fb9482ce
antonioparra7 <antonioparra00@gmail.com>
2021-05-18 14:04:03 +0200

    grafica comun añadida

--- src/front/globalGraphic.svelte
+++ src/front/globalGraphic.svelte
@@ -2,47 +2,47 @@
     import { onMount } from "svelte";
     import {Nav,NavItem,NavLink} from "sveltestrap";
     let BASE_API_URL = "/api/v2";
-    let BASE_API_URL_PAAWARDS = "/api/v1"
-
-    //Obtenemos los paises
-    let country = [];
-    async function getCountry(){
-        let paawards = [];
-        
-        const data = await fetch(BASE_API_URL_PAAWARDS + "/paawards");
-        paawards = await data.json();
-        paawards.forEach((x) => {
-            if (!country.includes(x.country)){
-                country.push(x.country);
-            }
-        });
-        console.log(country);
-        loadGraph();
-    }
+    let BASE_API_URL_PAAWARDS = "/api/v1";
 
     async function loadGraph(){
         let paawardsGraph = [];
-        for (const c of country){
-            let arrayAños = [];
-            for (var año=2000; año<2021; año++){
-                const res = await fetch(BASE_API_URL_PAAWARDS + `/paawards?country=${c}&year=${año}`);
-                if (res.ok) {
-                    const json = await res.json();
-                    var suma = 0;
-                    json.forEach((p) => {
-                        var trofeos = parseInt(p.trophy);
-                        suma=suma+trofeos;
-                    });
-                    arrayAños.push(suma);
-                } else {
-                    arrayAños.push(0);
-                }
+        let richppGraph = [];
+        var edad1 = 0;
+        for (var año=2016; año<2021; año++){
+            const res1 = await fetch(BASE_API_URL_PAAWARDS + `/paawards?year=${año}`);
+            const res2 = await fetch(BASE_API_URL+`/richpp?year=${año}&top=1`);
+            //PAAWARDS
+            if (res1.ok) {
+                const json1 = await res1.json();
+                var cont = 0;
+                var suma = 0.0;
+                json1.forEach((p) => {
+                    var edad = parseInt(p.age);
+                    suma=suma+edad;
+                    cont=cont+1;
+                });
+                var resultado1 = suma/cont;
+                paawardsGraph.push(resultado1);
+            } 
+            else {
+                paawardsGraph.push(0);
             }
-            paawardsGraph.push({name: c, data: arrayAños, pointPlacement: 'on'});
+            //RICHPP
+            if (res2.ok) {
+                const json2 = await res2.json();
+                json2.forEach((r) => {
+                    edad1 = parseInt(r.age);
+                });
+                richppGraph.push(edad1);
+            } 
+            else {
+                richppGraph.push(0);
+            }
         }
+    
         Highcharts.chart('container', {
             title: {
-                text: 'Premios Princesa de Asturias 2000-2020'
+                text: 'Analítica Común'
             },
 
             subtitle: {
@@ -51,14 +51,14 @@
 
             yAxis: {
                 title: {
-                    text: 'Total de premios'
+                    text: ''
                 }
 
             },
 
             xAxis: {
                 accessibility: {
-                    rangeDescription: 'Range: 2000 to 2020'
+                    rangeDescription: 'Range: 2016 to 2020'
                 }
             },
 
@@ -73,11 +73,19 @@
                     label: {
                         connectorAllowed: false
                     },
-                    pointStart: 2000
+                    pointStart: 2016
                 }
             },
-
-            series: paawardsGraph,
+            series: [
+                        {
+                          name: "Edad Ganador Premio Princesa Asturias",
+                          data: paawardsGraph,
+                        },
+                        {
+                          name: "Edad Top 1 en Fortuna",
+                          data: richppGraph,
+                        }
+            ],
 
             responsive: {
                 rules: [{
@@ -103,22 +111,22 @@
     <script src="https://code.highcharts.com/modules/series-label.js"></script>
     <script src="https://code.highcharts.com/modules/exporting.js"></script>
     <script src="https://code.highcharts.com/modules/export-data.js"></script>
-    <script src="https://code.highcharts.com/modules/accessibility.js" on:load="{getCountry}"></script>
+    <script src="https://code.highcharts.com/modules/accessibility.js" on:load="{loadGraph}"></script>
 </svelte:head>
 
 <main>
     <Nav>
         <NavItem>
-            <NavLink href="/#/paawards">Volver</NavLink>
+            <NavLink href="/#/info">Volver</NavLink>
         </NavItem>
     </Nav>
 
     <figure class="highcharts-figure">
         <div id="container"></div>
         <p class="highcharts-description">
-            Grafico que representa los ganadores de los Premios Princesa de Asturias desde el año 2000
-            junto a la edad con la que ganaron el premio y al número de trofeos que ha ganado a lo largo 
-            de su carrera
+            Grafico que representa la edad de los ganadores de los Premios Princesa de Asturias,
+            la edad de la persona con mayor fortuna en el mundo,
+            desde el año 2016 hasta 2020.
         </p>
     </figure>
 </main>
